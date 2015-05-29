5925a2e541cfdd81604cf34f539037a9a2dbbe64
Chris Bouchard <chris@upliftinglemma.net>
2015-05-29 00:04:08 -0400

    Actually compiles now.

--- src/application.rs
+++ src/application.rs
@@ -1,8 +1,8 @@
-use super::dispatcher::{HasDispatcher, SequenceEvent};
+use super::dispatcher::{Dispatcher, HasDispatcher, SequenceEvent};
 
 pub trait Application : HasDispatcher<SequenceEvent> + Sync {
-    fn sequencer<'a>(&'a self) -> &'a Dispatcher<'a, SequnceEvent> {
-        self.dispatcher();
+    fn sequencer<'a>(&'a self) -> &'a Dispatcher<'a, SequenceEvent> {
+        self.dispatcher()
     }
 }
 
