e5fc0eeffc309578ae699812bcccc7267d2e79a9
Chris Bouchard <chris@upliftinglemma.net>
2015-05-30 23:11:58 -0400

    We'll start calling controllers listeners.
    This is to avoid confusion in MVC applications.

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
 pub mod application;
-pub mod controller;
+pub mod listener;
 pub mod dispatcher;
 pub mod event;
 pub mod id_counter;
 pub mod queue_map;
 
 pub use application::Application;
-pub use controller::{Controller, Handler};
+pub use listener::{Controller, Handler};
 pub use dispatcher::{Broadcaster, Dispatcher, HasDispatcher, SequenceEvent, Sequencer};
 pub use event::Event;
 
